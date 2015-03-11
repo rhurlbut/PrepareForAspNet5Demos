113173099edf7d7e8c36f9906d1b571ff01adf12
rhurlbut <robert@roberthurlbut.com>
2015-03-11 09:38:20 -0400

    Adding two starter sample projects: ASPNET5_SampleMvc.kproj and ASPNET5_SampleWebAPI.kproj

---
+++ Source/ASPNET5_SampleMvc/Compiler/Preprocess/RazorPreCompilation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNet.Mvc;
+
+namespace ASPNET5_SampleMvc.Compiler.Preprocess
+{
+    public class RazorPreCompilation : RazorPreCompileModule
+    {
+        public RazorPreCompilation(IServiceProvider provider) : base(provider)
+        {
+        }
+    }
+}
